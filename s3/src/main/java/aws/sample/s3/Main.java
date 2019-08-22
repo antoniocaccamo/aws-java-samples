ad95fa646dd4d072234e8b980ab2f87c232ddfd6
antonio caccamo <caccamo.antonio@gmail.com>
2019-08-22 17:31:32 +0200

    Initial commit

--- s3/src/main/java/aws/sample/s3/Main.java
+++ s3/src/main/java/aws/sample/s3/Main.java
@@ -1,36 +1,58 @@
 package aws.sample.s3;
 
+import aws.sample.s3.command.S3Command;
+import aws.sample.s3.command.S3ListBucketCommand;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.ExitCodeGenerator;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import software.amazon.awssdk.services.s3.S3Client;
-import software.amazon.awssdk.services.s3.model.ListBucketsRequest;
-import software.amazon.awssdk.services.s3.model.ListBucketsResponse;
+import picocli.CommandLine;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * @author antoniocaccamo on 22/08/2019.
  */
 
 @SpringBootApplication @Slf4j
-public class Main implements CommandLineRunner{
+public class Main implements CommandLineRunner, ExitCodeGenerator {
+
+    private final CommandLine.IFactory factory;
+    private final S3Command s3Command;
+    private int exitCode;
 
     public static void main(String[] args) {
         log.info(">>> application started <<<");
-        SpringApplication.run(Main.class, args);
+        //SpringApplication.run(Main.class, args);
+        System.exit(SpringApplication.exit(SpringApplication.run(Main.class, args)));
         log.info(">>> application ended   <<<");
     }
 
+    public Main(CommandLine.IFactory factory, S3Command s3Command, S3ListBucketCommand s3ListBucketCommand) {
+        this.factory = factory;
+        this.s3Command = s3Command;
+    }
+
     @Override
-    public void run(String... strings) throws Exception {
-        log.info("creating S3 client..");
-        try (S3Client s3 = S3Client.builder().build()){
-            log.info("list buckets..");
-            ListBucketsRequest listBucketsRequest = ListBucketsRequest.builder().build();
-            ListBucketsResponse listBucketsResponse = s3.listBuckets(listBucketsRequest);
-            listBucketsResponse.buckets().stream().forEach(x -> System.out.println(x.name()));
-        }catch (Exception e){
-            log.error("error occurred", e);
-        }
+    public int getExitCode() {
+        return exitCode;
+    }
+
+//    @Override
+//    public void run(String... args) throws Exception {
+//        log.info("args : {}", args);
+////        CommandLine commandLine = new CommandLine(s3Command);
+////        commandLine.parseWithHandler(new CommandLine.RunLast(), strings);
+//        exitCode = new CommandLine(s3Command, factory).execute(args);
+//    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        CommandLine cl =  new CommandLine(s3Command, factory);
+        exitCode = cl.execute(args);
     }
 
 }
---
+++ s3/src/main/java/aws/sample/s3/command/S3ListBucketCommand.java
@@ -0,0 +1,34 @@
+package aws.sample.s3.command;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.ListBucketsRequest;
+import software.amazon.awssdk.services.s3.model.ListBucketsResponse;
+
+import java.util.concurrent.Callable;
+
+import static picocli.CommandLine.Command;
+
+/**
+ * @author antoniocaccamo on 22/08/2019.
+ */
+
+@Component
+@Command(name = "list", aliases = {"ls"}) @Slf4j
+public class S3ListBucketCommand implements Callable<Integer>{
+
+    @Override
+    public Integer call() throws Exception{
+    log.info("creating S3 client..");
+        try (S3Client s3 = S3Client.builder().build()){
+            log.info("list buckets..");
+            ListBucketsRequest listBucketsRequest = ListBucketsRequest.builder().build();
+            ListBucketsResponse listBucketsResponse = s3.listBuckets(listBucketsRequest);
+            listBucketsResponse.buckets().stream().forEach(x -> System.out.println(x.name()));
+        }catch (Exception e){
+            log.error("error occurred", e);
+        }
+        return 0;
+    }
+}
